bac43c0d7f829881d36a996bc609bf97abead28c
codeXp1908 <prakharsaxena3416@gmail.com>
2024-03-07 04:41:24 +0530

    Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import AuthContainer from './components/auth/AuthContainer';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./component/Pages/home/Home";
+import { NavbarDefault } from "./component/Navbar";
+import Login from "./component/login/Login";
+import Signup from "./component/login/Signup";
 
 function App() {
+
+
+
   return (
-    <>
-      <AuthContainer />
-    </>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<div>{<Home />}</div>} />
+        <Route path="/Login" element={<div>{<Login />}</div>} />
+        <Route path="/Signup" element={<div>{<Signup />}</div>} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
